6693a1a4b914cfe16024f4cc56a1c1c54930ffb1
Shahed Chowdhuri <shchowd@microsoft.com>
2020-09-03 02:38:06 -0400

    updated TF to create additional load balancer with Private IP + vnet with subnet

--- examples/terraform-azure-loadbalancer-example/main.tf
+++ examples/terraform-azure-loadbalancer-example/main.tf
@@ -22,7 +22,7 @@ resource "azurerm_resource_group" "main" {
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
-# DEPLOY LOAD BALANCER
+# DEPLOY LOAD BALANCER WITH PUBLIC IP 
 # ---------------------------------------------------------------------------------------------------------------------
 
 resource "azurerm_public_ip" "main" {
@@ -35,14 +35,48 @@ resource "azurerm_public_ip" "main" {
   idle_timeout_in_minutes = "4"
 }
 
-resource "azurerm_lb" "main" {
-  name                = "${var.prefix}-lb"
+resource "azurerm_lb" "main01" {
+  name                = "${var.prefix}-lb01"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   sku                 = "Standard"
 
     frontend_ip_configuration {
-      name                 = "${var.prefix}-frontendip"
+      name                 = "${var.prefix}-frontendip01"
       public_ip_address_id = azurerm_public_ip.main.id
+    }
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# DEPLOY LOAD BALANCER WITH PRIVATE IP 
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-vnet01"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  address_space       = ["10.200.0.0/21"]
+  dns_servers         = ["10.200.0.5", "10.200.0.6"]
+
+}
+
+resource "azurerm_subnet" "main" {
+  name                = "${var.prefix}-subnet01"
+  resource_group_name = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefix     = "10.200.2.0/25"
+}
+
+resource "azurerm_lb" "main02" {
+  name                = "${var.prefix}-lb02"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku                 = "Standard"
+
+    frontend_ip_configuration {
+      name                 = "${var.prefix}-frontendip02"
+      subnet_id                     = azurerm_subnet.main.id
+      private_ip_address            = "10.200.2.10"
+      private_ip_address_allocation = "Static"
     }
 }
--- examples/terraform-azure-loadbalancer-example/outputs.tf
+++ examples/terraform-azure-loadbalancer-example/outputs.tf
@@ -3,5 +3,5 @@ output "resource_group_name" {
 }
 
 output "loadbalancer_name" {
-  value = azurerm_lb.main.name
+  value = azurerm_lb.main01.name
 }
