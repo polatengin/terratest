865b3db7e6f1891794d479fad5c0351f9792230f
Shahed Chowdhuri <shchowd@microsoft.com>
2020-09-04 12:43:51 -0400

    added additional config properties for testing LB01 and Public IP

--- modules/azure/loadbalancer.go
+++ modules/azure/loadbalancer.go
@@ -2,7 +2,6 @@ package azure
 
 import (
 	"context"
-	"os"
 
 	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2019-09-01/network"
 )
@@ -11,9 +10,8 @@ const (
 	AzureEnvironmentEnvName = "AZURE_ENVIRONMENT"
 )
 
-// ref: StorageAccountExistsE
-// GetLoadBalancerE returns an LB client
-func GetLoadBalancerE(loadBalancerName, resourceGroupName, subscriptionID string) (bool, error) {
+// LoadBalancerExistsE returns an LB client
+func LoadBalancerExistsE(loadBalancerName, resourceGroupName, subscriptionID string) (bool, error) {
 	subscriptionID, err := getTargetAzureSubscription(subscriptionID)
 	client, err := GetLoadBalancerClientE(subscriptionID)
 	lb, err := client.Get(context.Background(), resourceGroupName, loadBalancerName, "")
@@ -24,14 +22,50 @@ func GetLoadBalancerE(loadBalancerName, resourceGroupName, subscriptionID string
 	return *lb.Name == loadBalancerName, nil
 }
 
-// ref: GetStorageAccountClientE
-// GetStorageAccountClientE creates a storage account client.
+// GetLoadBalancerE returns a load balancer resource as specified by name.
+func GetLoadBalancerE(loadBalancerName string, resourceGroupName string, subscriptionID string) (*network.LoadBalancer, error) {
+	subscriptionID, err := getTargetAzureSubscription(subscriptionID)
+	client, err := GetLoadBalancerClientE(subscriptionID)
+	lb, err := client.Get(context.Background(), resourceGroupName, loadBalancerName, "")
+	if err != nil {
+		return nil, err
+	}
+
+	return &lb, nil
+}
+
+// GetLoadBalancerClientE creates a storage account client.
 func GetLoadBalancerClientE(subscriptionID string) (*network.LoadBalancersClient, error) {
-	loadBalancerClient := network.NewLoadBalancersClient(os.Getenv(AzureSubscriptionID))
+	loadBalancerClient := network.NewLoadBalancersClient(subscriptionID)
 	authorizer, err := NewAuthorizer()
 	if err != nil {
 		return nil, err
 	}
 	loadBalancerClient.Authorizer = *authorizer
 	return &loadBalancerClient, nil
+}
+
+// GetPublicIPAddressE will return ApplicationGateway object and an error object
+func GetPublicIPAddressE(resourceGroupName, publicIPAddressName, subscriptionID string) (*network.PublicIPAddress, error) {
+	subscriptionID, err := getTargetAzureSubscription(subscriptionID)
+	client, err := GetPublicIPAddressClientE(subscriptionID)
+	if err != nil {
+		return nil, err
+	}
+	publicIPAddress, err := client.Get(context.Background(), resourceGroupName, publicIPAddressName, "")
+	if err != nil {
+		return nil, err
+	}
+	return &publicIPAddress, nil
+}
+
+// GetPublicIPAddressClientE creates a PublicIPAddresses client
+func GetPublicIPAddressClientE(subscriptionID string) (*network.PublicIPAddressesClient, error) {
+	client := network.NewPublicIPAddressesClient(subscriptionID)
+	authorizer, err := NewAuthorizer()
+	if err != nil {
+		return nil, err
+	}
+	client.Authorizer = *authorizer
+	return &client, nil
 }
